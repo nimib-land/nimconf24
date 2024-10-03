1dc33267c227a0ec384e3a68357b3c6cd849d1fd
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2024-10-03 19:58:43 +0200

    work on about_nimislides

--- about_nimislides.nim
+++ about_nimislides.nim
@@ -1,5 +1,5 @@
 import nimib, nimislides
-import our
+import our, hugo
 
 template sectionSlide* =
   slide:
@@ -18,15 +18,55 @@ template docsExample1* =
   slide(slideOptions(iframeBackground="https://hugogranstrom.com/nimiSlides/tutorials/layout.html")):
     discard
 
+template autoAnimationShowcase* =
+  slide(slideOptions(autoAnimate=true)):
+    nbText: "## Auto-animation"
+  autoAnimateSlides(5):
+    nbText: "## Auto-animation"
+    showUntil(2):
+      nbText "Show until frame 2"
+    showAt(2, 4):
+      nbText: "Show at frame 2 & 4"
+    showFrom(3):
+      nbText: "Show from frame 3"
+    showText(@[({1..5}, "Frame: "), ({1}, "1"), ({2}, "2"), ({3}, "3"), ({4}, "4"), ({5}, "5")])
+
+template autoAnimateBasics* =
+  slide:
+    nimibCode:
+      autoAnimateSlides(5):
+        nbText: "## Auto-animation"
+        showUntil(2):
+          nbText "Show until frame 2"
+        showAt(2, 4):
+          nbText: "Show at frame 2 & 4"
+        showFrom(3):
+          nbText: "Show from frame 3"
+    
 
 
+template showAtSlides* =
+  slide:
+    autoAnimateSlides(5):
+      nbText: "### showAt"
+      nimibCode:
+        showAt(1, 5):
+          nbText: "1 & 5"
+        showAt(2..4):
+          nbText: "2..4"
+        showAt({3, 5}):
+          nbText: "{3, 5}"
+      showText(@[({1..5}, "Frame: "), ({1}, "1"), ({2}, "2"), ({3}, "3"), ({4}, "4"), ({5}, "5")])
 
 template all* =
   sectionSlide
   docs1
   docsExample1
+  autoAnimationShowcase
+  autoAnimateBasics
+  showAtSlides
 
 when isMainModule:
-  myInit("about_nimislides")
-  docsExample1
+  myInit("about_nimislides.nim")
+  all
   nbSave
