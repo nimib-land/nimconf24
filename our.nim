46c4b770eb9317df85c7ff9280ffc877f0b885aa
Hugo Granström <5092565+HugoGranstrom@users.noreply.github.com>
2024-10-03 19:58:02 +0200

    add nimconf theme

--- our.nim
+++ our.nim
@@ -16,9 +16,41 @@ template nbTextSmall*(text: string) =
 template reference*(text: string) =
   nbTextSmall: text
 
+template nimConfTheme*() =
+  setSlidesTheme(Black)
+  let nimYellow = "#FFE953"
+  nb.addStyle: """
+:root {
+  --r-background-color: #181922;
+  --r-heading-color: $1;
+  --r-link-color: $1;
+  --r-selection-color: $1;
+  --r-link-color-dark: darken($1 , 15%)
+}
+
+.reveal ul, .reveal ol {
+  display: block;
+  text-align: left;
+}
+
+li::marker {
+  color: $1;
+  content: "»";
+}
+
+li {
+  padding-left: 12px;
+}
+""" % [nimYellow]
+
+template nimConfSlide*(body: untyped) =
+  slide:
+    cornerImage("https://raw.githubusercontent.com/nimib-land/assets/refs/heads/main/nimib_logo_white_bg_4k.png", UpperRight, size=100, animate=false)
+    body
+
 template myInit*(sourceFileRel = "my.nim") =
   nbInit(thisFileRel=sourceFileRel, theme=revealTheme)
-  setSlidesTheme(Dracula)
+  nimConfTheme()
   addNbTextSmall
   nbRawHtml """
 <style>
