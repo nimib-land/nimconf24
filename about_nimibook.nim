097f720454231059c998cdccead2babb21f1fd09
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-10-12 10:02:58 +0200

    mdbook example + restructure draft

--- about_nimibook.nim
+++ about_nimibook.nim
@@ -17,13 +17,13 @@ template what* =
       nbText "how do we show a collection of documents?"
     showAt(4):
       nbText "enter Nimibook!"
-    showAt(5..8):
+    showAt(5..8, 10, 11):
       nbText "Nimibook is"
-    showAt(6..10):
+    showAt(6..9):
       nbText "1. a minimal **static site generator** for nimib"
-    showAt(7..10):
+    showAt(7..9):
       nbText "2. implemented using **theme** feature of nimib"
-    showAt(8..10):
+    showAt(8..11):
       nbText "3. a port of [mdbook]()"
     showAt(10..11):
       nbText "so..."
@@ -32,16 +32,72 @@ template what* =
     showFrom(12):
       nbText "1. a popular **static site generator** (🦀)"
     showFrom(13):
-      nbText "2. with a single book theme"
+      nbText "2. with a single book 'theme'"
     showFrom(14):
       nbText "3. and with minimal customization"
 
+template mdbookExample* =
+  slide:
+    nbText "### a mdbook example"
+  slide:
+    nbImage("nimdays.png")
+    nbText "[xmonader.github.io/nimdays](https://xmonader.github.io/nimdays/day16_asciitables.html)"
+  slide:
+    nbText """### SUMMARY.MD
+```md
+# Nim Days
+
+Documenting my days with The Nim Programming language.
+
+- [Intro](book_intro.md)
+- [Day 1 DMIDecode](day01_dmidecode.md)
+- [Day 2: Parsing Bencode](day02_bencode.md)
+- [Day 3: FFI and Libmagic](day03_libmagic.md)
+- ...
+```
+"""
+  slide:
+    nbText """### content.MD
+````md
+or let nim decide for you 
+
+```nim
+  t.tableWidth = 0
+  printTable(t)
+```
+
+```
++---+-------+---------+
+|ID |Name   |Date     |
++---+-------+---------+
+|1  |Aaaa   |2018-10-2|
++---+-------+---------+
+```
+````
+"""
+
+template nimibookExample* =
+  slide:
+    nbText "### A nimibook example"
+
+template howIstart* =
+  slide:
+    nbText "### How I start"
+
+template howIcontribute* =
+  slide:
+    nbText "### How I contribute"
+
 template all* =
   sectionSlide
-  what
+  slide:
+    what
+    mdbookExample
+  nimibookExample
+  howIstart
+  howIcontribute
 
 when isMainModule:
   myInit("about_nimibook")
-  what
-  #all
+  all
   nbSave
--- notes.md
+++ notes.md
@@ -1,3 +1,45 @@
+# dev notes
+
+## nimibook content draft
+
+- what is nimibook [x]
+- example of mdbook (nimdays)  [x]
+  - summary.md [x]
+  - content.md [x]
+- example of nimibook (scinim)
+  - toc.nim
+  - content.nim
+- how to start
+  - start with a toc
+  - nim r toc init
+  - nim r toc build
+  - single file build with nim r toc dump
+  - more on nimibook (in nimibook)
+- how to contribute
+  - create and share content (list examples)
+  - see code guide for more
+
+## plan for Sat Oct 12
+
+- watched both videos by Hugo (7' intro to nimib, 10' news from nimislides) [x]
+  - made me think: order of stuff and transitions (and target durations) [x]
+    - 2' title, agenda (both recording together)
+      - transition with Pietro saying: Hugo will now do a very short introduction to Nimib for those who do not know it
+    - 7' intro to nimib (hugo)
+    - X nimib speaking hours (both)
+      - includes intro to nimibland and nimib.py by pietro
+      - transitions with Pietro introducing Hugo
+    - 10' news from nimislides (hugo)
+    - Y intro to nimibook (pietro)
+    - 1' thank you and goodbye
+    - total: 20' + X + Y
+      - try to make X <= 5' and Y <= 10' to have total <= 35'
+- nimibook presentation
+  - finalize slides
+  - record a decent take
+- schedule a moment to record with Hugo
+
+
 ## session Fri Sep 20
 
 title: updates from NimibLand
